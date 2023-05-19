9959e2c6b435c8fcd9d55e8a2777e4044866e640
huyha01122021 <huyha01072002@gmail.com>
2023-05-19 18:48:45 +0700

    lần đầu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './Global.scss';
 import Header from './Component/Header';
+// import Logo1 from '../src/ases/logo-Tiktok.jpg'
 // import './Global.scss';
 function App() {
   return (
     <div className="App">
-  
      <Header />
-
+ 
     </div>
   );
 }
--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,9 +1,11 @@
+import Logo1 from '../../src/ases/logo-Tiktok.jpg'
 function Header(){
     return(
         <div>
-         <div className="logo"></div>
-         <div></div>
-         <div></div>
+         <div className="logo">
+
+         <img className='logo1' src={Logo1} />
+         </div>
         </div>
     )
 }
